833d1ccbe969d2ef5a1c0884d113c5afb01b62b4
pro 2020 <pro2020@pros-MacBook-Pro.local>
2025-11-17 23:19:30 +0100

    day 3

---
+++ week1/day2/functions.py
@@ -0,0 +1,8 @@
+message = "How are u doing"
+def greet():
+    message = "my love"
+    print(message)
+
+
+greet()
+print(message + " outside")
---
+++ week1/day2/loops.py
@@ -0,0 +1,10 @@
+sequence = "Helloworld"
+
+for char in sequence:
+    print(char)
+
+# the for loop iterates over items in the sequence
+count = 5
+while count >0:
+    print(count)
+    count -= 1
